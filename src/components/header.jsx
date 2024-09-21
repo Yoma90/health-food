67d0bc490e290d3491e40509d2bf30e03f8ef2b7
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-09-21 09:14:38 +0400

    T3: Add base layout

---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import { ShoppingBasket } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export const Header = () => {
+  return (
+    <header className="flex items-center gap-2 px-4 py-2 border-b shadow-sm">
+      <Link href="/" className="inline-flex items-center gap-2">
+        <Image
+          src="/healthdonals.png"
+          alt="Healthdonals"
+          width={32}
+          height={32}
+        />
+        <p className="text-sm font-bold">Healthdonals</p>
+      </Link>
+      <div className="ml-auto"></div>
+      <Button
+        size="sm"
+        variant="outline"
+        className="inline-flex gap-2 items-center"
+      >
+        0
+        <ShoppingBasket size={12} />
+      </Button>
+    </header>
+  );
+};
