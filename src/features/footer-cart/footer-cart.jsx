2a3b05613ba98b2677b06cdb041492f6061a81c4
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-09-21 11:31:46 +0400

    T10: Add checkout page

--- src/features/footer-cart/footer-cart.jsx
+++ src/features/footer-cart/footer-cart.jsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format-price";
-import { useCartPrice, useCartStore } from "@/lib/store/use-cart-store";
-import { ChevronDown, ChevronUp, Minus, Trash } from "lucide-react";
+import { useCartPrice } from "@/lib/store/use-cart-store";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
+import { ItemsCart } from "../cart/items-cart";
 
 export const FooterCart = () => {
   const [open, setOpen] = useState(false);
 
-  const cart = useCartStore();
   const price = useCartPrice();
 
   return (
@@ -22,51 +23,18 @@ export const FooterCart = () => {
       >
         {open ? <ChevronDown size={12} /> : <ChevronUp size={12} />}
       </Button>
-      {open ? (
-        <>
-          <div className="flex items-center gap-4">
-            <h2 className="text-lg font-bold">Cart</h2>
-            <p className="ml-auto font-mono">{formatPrice(price)}</p>
-          </div>
-          <div className="flex max-h-32 flex-col gap-2 overflow-y-auto py-2">
-            {Object.values(cart.items).map((cartItem) => (
-              <CartLineItem
-                quantity={cartItem.quantity}
-                item={cartItem.item}
-                key={cartItem.item.id}
-              />
-            ))}
-          </div>
-        </>
-      ) : null}
+      {open ? <ItemsCart /> : null}
       <div className="flex items-center gap-2">
-        <Button className="w-full" size="sm">
+        <Link
+          href="/checkout"
+          className={buttonVariants({ size: "sm", className: "w-full" })}
+        >
           Checkout
-        </Button>
+        </Link>
         {!open ? (
           <p className="ml-auto font-mono">{formatPrice(price)}</p>
         ) : null}
       </div>
-    </div>
-  );
-};
-const CartLineItem = ({ item, quantity }) => {
-  const removeItem = useCartStore((s) => s.removeItem);
-  return (
-    <div className="flex items-center gap-4">
-      <div className="relative size-14 rounded-md border bg-accent/50 p-1">
-        <img src={item.image} alt={`${item.name}'s image`} />
-        <span className="absolute -right-2 -top-2 flex size-5 items-center justify-center rounded-full border bg-accent text-sm">
-          {quantity}
-        </span>
-      </div>
-      <p className="font-bold">{item.name}</p>
-      <p className="ml-auto font-mono text-sm">
-        {formatPrice(item.price * quantity)}
-      </p>
-      <Button size="sm" variant="outline" onClick={() => removeItem(item)}>
-        {quantity === 1 ? <Trash size={12} /> : <Minus size={12} />}
-      </Button>
     </div>
   );
 };
